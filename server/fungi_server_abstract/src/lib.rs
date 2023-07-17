fdf54f1dc8ef02f24bcc780e9e7339a564713920
csf <csf@enbop.com>
2023-07-17 17:32:43 +0900

    basic server abstract

--- server/fungi_server_abstract/src/lib.rs
+++ server/fungi_server_abstract/src/lib.rs
@@ -1,5 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![feature(async_fn_in_trait)]
+
+use std::{error, fmt};
+
+#[derive(Debug)]
+enum ServerError {
+    AlreadyStarted,
+    NotStarted,
+    AlreadyStopped,
+    NotStopped,
+    Unknown,
+    Other(String),
+}
+
+impl fmt::Display for ServerError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ServerError::AlreadyStarted => write!(f, "Server already started"),
+            ServerError::NotStarted => write!(f, "Server not started"),
+            ServerError::AlreadyStopped => write!(f, "Server already stopped"),
+            ServerError::NotStopped => write!(f, "Server not stopped"),
+            ServerError::Unknown => write!(f, "Unknown error"),
+            ServerError::Other(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+impl error::Error for ServerError {}
+
+trait Verifier {
+    async fn verify<ID>(&self, identity: ID) -> bool; // TODO Error type, such as TooManyAttempts
+}
+
+trait ServerAbstract {
+    type VerifierImpl: Verifier;
+
+    async fn start(&self) -> Result<(), ServerError>;
+    async fn stop(&self) -> Result<(), ServerError>;
+
+    fn get_verifier(&self) -> &Self::VerifierImpl;
+
+    async fn verify<ID>(&self, identity: ID) -> Result<bool, ServerError> {
+        Ok(self.get_verifier().verify(identity).await)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +49,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
