683c0dd6afcaefff21a3ccc8943d262e1086d64f
csf <csf@enbop.com>
2023-07-18 22:23:14 +0900

    start tcp tls listener

--- server/fungi-server-simple-impl/src/listener/tcp.rs
+++ server/fungi-server-simple-impl/src/listener/tcp.rs
@@ -1,8 +1,11 @@
 use rustls_pemfile::{certs, rsa_private_keys};
+use tokio_rustls::server::TlsStream;
 use std::fs::File;
 use std::io::{self, BufReader};
+use std::net::SocketAddr;
 use std::path::{Path, PathBuf};
-use tokio::net::TcpListener;
+use std::sync::Arc;
+use tokio::net::{TcpListener, TcpStream};
 use tokio_rustls::rustls::{self, Certificate, PrivateKey};
 use tokio_rustls::TlsAcceptor;
 
@@ -18,3 +21,31 @@ fn load_keys(path: &Path) -> io::Result<Vec<PrivateKey>> {
         .map(|mut keys| keys.drain(..).map(PrivateKey).collect())
 }
 
+pub async fn start_tcp_tls_listener(
+    addr: &str,
+    cert_path: &PathBuf,
+    key_path: &PathBuf,
+    on_stream: impl Fn(TlsStream<TcpStream>, SocketAddr) + Send + Copy + Sync + 'static,
+) -> io::Result<()> {
+    let certs = load_certs(cert_path)?;
+    let mut keys = load_keys(key_path)?;
+
+    let config = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, keys.remove(0))
+        .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))?;
+
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+
+    let listener = TcpListener::bind(addr).await?;
+    loop {
+        let (socket, addr) = listener.accept().await?;
+        let acceptor = acceptor.clone();
+        tokio::spawn(async move {
+            if let Ok(socket) = acceptor.accept(socket).await {
+                on_stream(socket, addr);
+            }
+        });
+    }
+}
