8e9257751dd73ab73d5d9b475f42686f5c2ebaf5
Heap-Hop <csf@enbop.com>
2024-05-14 16:07:37 +0900

    remove global static

--- fungi-gateway/src/libp2p.rs
+++ fungi-gateway/src/libp2p.rs
@@ -2,81 +2,102 @@ use fungi_util::{AsyncResult, Completer};
 use libp2p::{
     futures::StreamExt, noise, ping, swarm::SwarmEvent, tcp, yamux, Multiaddr, PeerId, Swarm,
 };
-use std::{io, sync::OnceLock};
 use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
 
-// TODO: only start the swarm once
-static ASYNC_SWARM_CALLER: OnceLock<UnboundedSender<AsyncSwarmRequests>> = OnceLock::new();
-
-enum AsyncSwarmRequests {
+enum AsyncSwarmRequest {
     GetLocalPeerId(Completer<PeerId>),
 }
 
-// TODO: error handling
-pub async fn start_libp2p_swarm() {
-    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
-        .with_tokio()
-        .with_tcp(
-            tcp::Config::default(),
-            noise::Config::new,
-            yamux::Config::default,
-        )
-        .unwrap()
-        .with_behaviour(|_| ping::Behaviour::default())
-        .unwrap()
-        .build();
+pub struct SwarmState {
+    async_swarm_caller: UnboundedSender<AsyncSwarmRequest>,
+    swarm_task: tokio::task::JoinHandle<()>,
+}
 
-    swarm
-        .listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap())
-        .unwrap();
+impl SwarmState {
+    // TODO: error handling
+    // TODO: configurable
+    pub async fn start_libp2p_swarm() -> Result<Self, String> {
+        let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+            .with_tokio()
+            .with_tcp(
+                tcp::Config::default(),
+                noise::Config::new,
+                yamux::Config::default,
+            )
+            .map_err(|e| format!("Failed to build swarm: {:?}", e))?
+            .with_quic()
+            .with_behaviour(|_| ping::Behaviour::default())
+            .map_err(|e| format!("Failed to build swarm: {:?}", e))?
+            .build();
 
-    let (async_swarm_request_tx, mut async_swarm_request_rx) =
-        unbounded_channel::<AsyncSwarmRequests>();
+        swarm
+            .listen_on(
+                "/ip4/0.0.0.0/tcp/0"
+                    .parse()
+                    .expect("address should be valid"),
+            )
+            .map_err(|e| format!("Failed to listen on address: {:?}", e))?;
+        swarm
+            .listen_on(
+                "/ip4/0.0.0.0/udp/0/quic-v1"
+                    .parse()
+                    .expect("address should be valid"),
+            )
+            .map_err(|e| format!("Failed to listen on address: {:?}", e))?;
 
-    ASYNC_SWARM_CALLER.set(async_swarm_request_tx).unwrap();
+        let (async_swarm_request_tx, async_swarm_request_rx) =
+            unbounded_channel::<AsyncSwarmRequest>();
 
-    loop {
-        tokio::select! {
-            swarm_events = swarm.select_next_some() => {
-                match swarm_events {
-                    SwarmEvent::NewListenAddr { address, .. } => log::info!("Listening on {address:?}"),
-                    SwarmEvent::Behaviour(event) => log::info!("{event:?}"),
-                    _ => {}
+        let swarm_task = tokio::spawn(Self::start_swarm_task(swarm, async_swarm_request_rx));
+
+        Ok(Self {
+            async_swarm_caller: async_swarm_request_tx,
+            swarm_task,
+        })
+    }
+
+    async fn start_swarm_task(
+        mut swarm: Swarm<ping::Behaviour>,
+        mut async_swarm_request_rx: tokio::sync::mpsc::UnboundedReceiver<AsyncSwarmRequest>,
+    ) {
+        loop {
+            tokio::select! {
+                swarm_events = swarm.select_next_some() => {
+                    match swarm_events {
+                        SwarmEvent::NewListenAddr { address, .. } => log::info!("Listening on {address:?}"),
+                        SwarmEvent::Behaviour(event) => log::info!("{event:?}"),
+                        _ => {}
+                    }
+                },
+                async_request = async_swarm_request_rx.recv() => {
+                    let Some(async_request) = async_request else {
+                        log::warn!("AsyncSwarmRequest channel closed");
+                        break;
+                    };
+                    Self::handle_async_request(&mut swarm, async_request).await;
                 }
-            },
-            async_request = async_swarm_request_rx.recv() => {
-                let Some(async_request) = async_request else {
-                    log::warn!("AsyncSwarmRequest channel closed");
-                    break;
-                };
-                handle_async_request(&mut swarm, async_request).await;
+            }
+        }
+    }
+
+    async fn handle_async_request(
+        swarm: &mut Swarm<ping::Behaviour>,
+        async_call_swarm: AsyncSwarmRequest,
+    ) {
+        match async_call_swarm {
+            AsyncSwarmRequest::GetLocalPeerId(completer) => {
+                let local_peer_id = swarm.local_peer_id();
+                completer.complete(local_peer_id.to_owned());
             }
         }
     }
 }
 
-async fn handle_async_request(
-    swarm: &mut Swarm<ping::Behaviour>,
-    async_call_swarm: AsyncSwarmRequests,
-) {
-    match async_call_swarm {
-        AsyncSwarmRequests::GetLocalPeerId(completer) => {
-            let local_peer_id = swarm.local_peer_id();
-            completer.complete(local_peer_id.to_owned());
-        }
-    }
-}
-
-pub struct AsyncSwarmRequest;
-use AsyncSwarmRequests::*;
-
-impl AsyncSwarmRequest {
-    pub async fn get_peer_id() -> PeerId {
+impl SwarmState {
+    pub async fn get_peer_id(&self) -> PeerId {
         AsyncResult::new(|completer| {
-            ASYNC_SWARM_CALLER
-                .get()
-                .unwrap()
-                .send(GetLocalPeerId(completer))
+            self.async_swarm_caller
+                .send(AsyncSwarmRequest::GetLocalPeerId(completer))
                 .unwrap();
         })
         .wait()
--- fungi/src/commands/daemon.rs
+++ fungi/src/commands/daemon.rs
@@ -1,21 +1,20 @@
 use std::time::Duration;
-
-use fungi_gateway::{start_libp2p_swarm, AsyncSwarmRequest};
+use fungi_gateway::SwarmState;
 
 pub async fn daemon() {
     println!("Starting Fungi daemon...");
-    tokio::spawn(start_libp2p_swarm());
+    let swarm = SwarmState::start_libp2p_swarm().await.unwrap();
 
     loop {
         tokio::select! {
             _ = tokio::time::sleep(Duration::from_secs(2)) => {
-                let peer_id = AsyncSwarmRequest::get_peer_id().await;
+                let peer_id = swarm.get_peer_id().await;
                 log::debug!("Peer ID: {:?}", peer_id);
             }
             _ = tokio::signal::ctrl_c() => {
                 println!("Shutting down Fungi daemon...");
                 break;
             }
-        }        
+        }
     }
 }
