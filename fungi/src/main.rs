ce4cb2add058dd8e19e628a98323d8d9d668b84d
Heap-Hop <csf@enbop.com>
2024-06-03 11:33:34 +0900

    fix init, add test

--- fungi/src/main.rs
+++ fungi/src/main.rs
@@ -8,7 +8,11 @@ use fungi::{
 async fn main() {
     env_logger::init();
     let args = FungiArgs::parse();
-    let config = FungiConfig::parse_from_dir(&args.fungi_dir()).unwrap();
+    let config = match args.command {
+        Some(Commands::Init) => FungiConfig::default(),
+        _ => FungiConfig::parse_from_dir(&args.fungi_dir()).unwrap(),
+    };
+
     match args.command {
         Some(Commands::Init) => commands::init(&args),
         Some(Commands::Daemon) => commands::daemon(&args, &config).await,
