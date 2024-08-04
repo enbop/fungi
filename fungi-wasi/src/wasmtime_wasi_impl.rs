1472c3d97092cdaea5170e9048a6541bd1822aef
en-bop <hi@enbop.com>
2024-08-04 13:42:47 +0900

    add error context

--- fungi-wasi/src/wasmtime_wasi_impl.rs
+++ fungi-wasi/src/wasmtime_wasi_impl.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Result};
+use anyhow::{bail, Context, Result};
 use bytes::Bytes;
 use std::path::PathBuf;
 use tokio::sync::mpsc;
@@ -62,10 +62,10 @@ impl WasiRuntime {
     pub fn new(root_dir: PathBuf, bin_dir: PathBuf) -> Result<Self> {
         let mut config = Config::new();
         config.async_support(true);
-        let engine = Engine::new(&config)?;
+        let engine = Engine::new(&config).context("failed to create engine")?;
 
         let mut linker: Linker<WasiP1Ctx> = Linker::new(&engine);
-        preview1::add_to_linker_async(&mut linker, |t| t)?;
+        preview1::add_to_linker_async(&mut linker, |t| t).context("failed to add linker")?;
 
         Ok(Self {
             engine,
@@ -80,7 +80,7 @@ impl WasiRuntime {
         // find bin in bin_dir
         let bin_path = self.bin_dir.join(bin);
         if !bin_path.exists() {
-            bail!("{:?} not found", bin_path);
+            bail!("failed to run cmd, {:?} not found", bin_path);
         }
 
         let (stdout, mut stdout_rx) = StdoutChannel::new();
@@ -98,16 +98,22 @@ impl WasiRuntime {
 
         let mut store = Store::new(&self.engine, wasi_ctx);
 
-        let module = Module::from_file(&self.engine, bin_path)?;
+        let module =
+            Module::from_file(&self.engine, bin_path).context("failed to create module")?;
         let func = self
             .linker
             .module_async(&mut store, "", &module)
-            .await?
-            .get_default(&mut store, "")?
-            .typed::<(), ()>(&store)?;
+            .await
+            .context("failed to link module")?
+            .get_default(&mut store, "")
+            .context("failed to get default function")?
+            .typed::<(), ()>(&store)
+            .context("failed to get typed function")?;
 
         // Invoke the WASI program default function.
-        func.call_async(&mut store, ()).await?;
+        func.call_async(&mut store, ())
+            .await
+            .context("failed to call function")?;
         Ok(())
     }
 }
