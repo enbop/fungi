cdb789bbbfdc06f9b119555e9a542ea5196d4151
Heap-Hop <csf@enbop.com>
2025-06-24 17:15:59 +0900

    fix fungi config init

--- crates/config/src/init.rs
+++ crates/config/src/init.rs
@@ -1,19 +1,19 @@
-use crate::{DEFAULT_CONFIG_FILE, FungiDir};
-use std::{io, path::Path};
+use crate::{FungiConfig, FungiDir};
+use anyhow::Result;
 
-pub fn init(dirs: &impl FungiDir) -> io::Result<()> {
+pub fn init(dirs: &impl FungiDir) -> Result<()> {
     let fungi_dir = dirs.fungi_dir();
     // check if the directory exists
-    // TODO also check if the directory is empty
-    if fungi_dir.exists() {
-        return Ok(());
+    if fungi_dir.exists() && fungi_dir.is_dir() {
+        if fungi_dir.read_dir()?.next().is_some() {
+            return Ok(());
+        }
     }
     println!("Initializing Fungi...");
     std::fs::create_dir(&fungi_dir)?;
 
     // create config.toml
-    let config = &fungi_dir.join(DEFAULT_CONFIG_FILE);
-    std::fs::File::create(config)?;
+    FungiConfig::apply_from_dir(&fungi_dir)?;
 
     // create .keys
     fungi_util::keypair::init_keypair(&fungi_dir)?;
