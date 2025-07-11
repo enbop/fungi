09e78ca749882aa9134af0a3be6d08d8828c9c73
Heap-Hop <csf@enbop.com>
2025-07-12 00:56:34 +0900

    cli: rm fungi_main

--- fungi/src/commands/mod.rs
+++ fungi/src/commands/mod.rs
@@ -1,18 +1,14 @@
 pub mod fungi_daemon;
-pub mod fungi_main;
 use clap::{Parser, Subcommand};
-use fungi_config::{DEFAULT_FUNGI_DIR, FungiDir};
-use std::path::PathBuf;
 
 /// Fungi the world!
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
 pub struct FungiArgs {
     #[command(subcommand)]
-    pub command: Option<Commands>,
-
-    #[arg(short, long)]
-    pub fungi_dir: Option<String>,
+    pub command: Commands,
+    // #[arg(short, long)]
+    // pub fungi_dir: Option<String>,
 }
 
 #[derive(Subcommand, Debug, Clone)]
@@ -21,11 +17,11 @@ pub enum Commands {
     Daemon(fungi_daemon::DaemonArgs),
 }
 
-impl FungiDir for FungiArgs {
-    fn fungi_dir(&self) -> PathBuf {
-        self.fungi_dir
-            .as_ref()
-            .map(PathBuf::from)
-            .unwrap_or_else(|| home::home_dir().unwrap().join(DEFAULT_FUNGI_DIR))
-    }
-}
+// impl FungiDir for FungiArgs {
+//     fn fungi_dir(&self) -> PathBuf {
+//         self.fungi_dir
+//             .as_ref()
+//             .map(PathBuf::from)
+//             .unwrap_or_else(|| home::home_dir().unwrap().join(DEFAULT_FUNGI_DIR))
+//     }
+// }
