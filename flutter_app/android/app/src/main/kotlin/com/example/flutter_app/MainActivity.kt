bb6322f46370a9244d3a7bfc774e589e4be072ed
Heap-Hop <csf@enbop.com>
2025-11-29 20:22:24 +0900

    refactor: mv flutter_app to a new repository https://github.com/enbop/fungi-app

--- flutter_app/android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package rs.fungi.fungi_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_app/lib/ui/utils/mobile_info.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-import 'package:flutter_foreground_task/flutter_foreground_task.dart';
-
-Future<void> initMobile() async {
-  if (Platform.isAndroid) {
-    FlutterForegroundTask.initCommunicationPort();
-  }
-}
